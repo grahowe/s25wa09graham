8b5c69453b0c3e9825bb839a96d2461205267a89
Owen Graham <omgraham02@gmail.com>
2025-03-24 20:24:53 -0500

    Oops...let's try that again

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/mydata');
+var mydataRouter = require('./routes/mydata');
 
 var app = express();
 
